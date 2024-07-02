0a8fac63431968399c26db53cc3dffac2ab8af67
Antonio Dorado <42452583+ndorado1@users.noreply.github.com>
2024-07-02 00:36:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if uploaded_file is not None:
     if not summary.empty and summary['Count of RA Action ID'].dtype in ['int64', 'float64']:
         # Mostrar el resumen en la aplicación
         st.subheader('Resumen de Licencias')
-        st.dataframe(summary.set_index('License Number').reset_index(drop=True), width=1000)
+        st.dataframe(summary, width=1000)
 
         # Generar gráfico de barras apiladas
         fig, ax = plt.subplots(figsize=(15, len(summary) * 0.5))  # Ajustar tamaño del gráfico
